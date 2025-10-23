9df4bdf8576f213e8bbc733f856652f1cc926c4e
SyukronM12 <msyukronm12@gmail.com>
2025-10-23 16:26:44 +0700

    add models

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
+
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
 }
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    protected $fillable = ['title', 'content', 'location_id', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function reporter()
+    {
+        return $this->belongsTo(User::class, 'reporter_id');
+    }
 }
